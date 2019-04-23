cfca02aae8384d40a86744ddec1b229b31e9a1e5
mvered <40322054+mvered@users.noreply.github.com>
2019-04-23 10:12:16 -0600

    Edited comments to clarify use

--- generate-tableau-files.sh
+++ generate-tableau-files.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-# Creates Tableau files with membership match reports for each partner from template
+# Creates new Tableau files from template which use new data sources
 
-# author: Michelle Vered <mvered@mtvoices.org>
+# author: Michelle Vered 
 # created: 3/28/19
 
 # Requirements to use:
-# 1) Template Tableau project file, named as test.twb
-# 2) Template Tableau project must use data source test.xlsx for membership data
-# 3) Data files for individual orgs membership data must be formatted exactly the same as test.xlsx
-# 4) Data files must be saved in the same folder as test.xlsx using the naming convention partnername.xlsx
+# 1) Template Tableau project file and data source named as template.twb and template.xlsx
+# 2) Additional data sources which have the same structure as template.xlsx
 
-# Instructions:
-# 1) Replace partner1name, partner2name, etc. with real partner names.
-# Partner names must be written the same as in the excel file names.
-# You can list as many partner names as you want. Save and close this file.
-# 2) Open a bash shell in the folder your Tableau and data files are saved in and type:
-# chmod +x generate-tableau-files.sh
-# ./generate-tableau-files.sh
-# 3) That's it!
-
-for partner in northernplains #replace with real partner names here
+for d in newdatasource1 newdatasource2 #replace with names of new data sources here
 do
-	sed "s/testdata\.xlsx/${partner}\.xlsx/" test.twb > ${partner}.twb
-	echo "${partner} Tableau file generated"
+	sed "s/template\.xlsx/${d}\.xlsx/" template.twb > ${d}.twb
+	echo "${d} Tableau file generated"
 done
